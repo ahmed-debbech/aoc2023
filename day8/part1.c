441e8d070bc7c0b6ed88d21f6a6c85c0601dde2c
ahmed-debbech <debbech.ahmed@gmail.com>
2023-12-08 14:01:41 +0100

    write day 8

--- day8/part1.c
+++ day8/part1.c
@@ -1,10 +1,22 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-struct Object{
-    char nav[50];
-    
+
+struct NetCell{
+    char from[3];
+    char right[3];
+    char left[3];
 };
+
+typedef struct NetCell NetCell;
+
+struct Object{
+    char nav[10000];
+    int count_nav;
+    NetCell network[10000];
+    int net_capacity;
+};
+
 typedef struct Object Object;
 
 Object * parse(){
@@ -21,11 +33,25 @@ Object * parse(){
     char * line = NULL;
     size_t len = 0;
     size_t read;
-    
+    read = getline(&line, &len, ptr);
+    b->count_nav = strlen(line);
+    strcpy(b->nav, line);
+
+    read = getline(&line, &len, ptr);
+
+    b->net_capacity = 0;
     while ((read = getline(&line, &len, ptr)) != -1) {
-        printf("Retrieved line of length %zu:\n", read);
-        printf("%s", line);
+        //printf("%s\n", line);
+        char from[3];
+        char right[3];
+        char left[3];
+        sscanf(line, "%s = (%s, %s)\n", from, left, right);
+        strcpy(b->network[b->net_capacity].from, from);
+        strcpy(b->network[b->net_capacity].left, left);
+        strcpy(b->network[b->net_capacity].right, right);
+        (b->net_capacity)++;
     }
+    fclose(ptr);
     return b;
 }
 
@@ -33,5 +59,6 @@ void main(){
     printf("hey\n");
 
     Object * obj = parse();
-    printf("obj %s\n", obj->nav);
+    printf("llll\n");
+    //printf("%s = (%s, %s)\n", obj->network[0].from, obj->network[0].right, obj->network[0].left);
 }
